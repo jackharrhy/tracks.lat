08b23c99be4a940fcf93eedcc9c18c0bf44945ba
Jack Arthur Harrhy <me@jackharrhy.com>
2023-08-21 20:59:04 -0230

    redirect / to /lon/

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -10,7 +10,12 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
+async def root():
+    return RedirectResponse("/lon/")
+
+
+@app.get("/lon/", response_class=HTMLResponse)
 def read_root(request: Request):
     return templates.TemplateResponse(
         "index.html",
